ef8a4039d1e1603106efb250c19e7c1ba60460b2
gflouris <flourisj@gmail.com>
2017-06-29 11:37:51 +0200

    cleanup

--- script/mimeemail.py
+++ script/mimeemail.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python -u
 
 import os, sys, re, time, commands, glob
-from optparse import OptionParser, OptionGroup
-from threading import Thread
-import random
 import smtplib
-import email
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
-from email.MIMEImage import MIMEImage
 from email import Encoders
 
 def send_mail(attachement):
@@ -22,21 +17,15 @@ def send_mail(attachement):
 	#        me = 'cctrack@mail.cern.ch'
 	#        me = 'threus@cern.ch'
 	me = 'gflouris@cern.ch'
-#### 2012 operation
-#	topeople = ['threus@cern.ch','cms-tracker-doc@cern.ch','francesco.palmonari@cern.ch','borrell@mail.cern.ch','lino.demaria@cern.ch','mia.tosi@cern.ch','erik.butz@cern.ch']
 #### 2013 commissioning
 #	topeople = ['threus@cern.ch','cms-tracker-doc@cern.ch','francesco.palmonari@cern.ch','mia.tosi@cern.ch','erik.butz@cern.ch','Christian.Barth@cern.ch']
-#### 201 AGR
-#	topeople = ['threus@cern.ch','cms-tracker-doc@cern.ch','francesco.palmonari@cern.ch','erik.butz@cern.ch','Christian.Barth@cern.ch']
-### TESTING
-	#topeople = ['threus@cern.ch']
+	
 	topeople = ['gflouris@cern.ch']
 	msg = MIMEMultipart()
 	msg['From'] = me
 	msg['To']   = COMMASPACE.join(topeople)
-	#	msg['To']   = me
-	msg['Subject'] = 'TKDoc Notification: RUN ' + str(runnumber)
-
+	msg['Subject'] = 'TKDoc Notification: RUN ' + str(runnumber)	
+	
 	msg.attach( MIMEText(emailmessage) )
 
 	isnewbadmodule = os.path.getsize('tkmap_bm_input.txt')
@@ -48,14 +37,7 @@ def send_mail(attachement):
 		part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attachement))
 		msg.attach(part)
 
-#        fp = open(attachement, 'rb')
-#	img = MIMEImage(fp.read())
-#	fp.close()
-#	msg.attach(img)
-
-
 	smtp = smtplib.SMTP(server)
-	#        smtp.sendmail(me, [me], msg.as_string())
 	smtp.sendmail(me, topeople, msg.as_string())
 	smtp.close()
 
