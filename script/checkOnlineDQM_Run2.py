b72bae4b6d6d232f773e5b06f8b9a5666bb2f1ab
gflouris <flourisj@gmail.com>
2017-07-07 15:50:07 +0200

    check number of alarms in an hour

--- script/checkOnlineDQM_Run2.py
+++ script/checkOnlineDQM_Run2.py
@@ -1,18 +1,22 @@
 from DQMInterface import *
+from mimeemail import *
 import sqlite3
 import datetime
+
 
 conn = sqlite3.connect('alarms_log.db')
 dbcursor = conn.cursor()
 try:
-    dbcursor.execute('SELECT EXISTS(SELECT 1 FROM alarms WHERE id=0 )')
+    dbcursor.execute('SELECT EXISTS(SELECT 1 FROM alarms WHERE id=1 )')
 except sqlite3.Error as exerror:
     if(exerror):
         dbcursor.execute('''CREATE TABLE alarms(id integer PRIMARY KEY,date, run int, lumi int, dead_value int, DataPresent)''')
 
 serverurl = 'https://cmsweb.cern.ch/dqm/online'
 
-DQMMon = DQMInterface(serverurl, 297726)
+DQMMon = DQMInterface(serverurl, 297702)
+
+
 
 if(DQMMon.onlinePublishing):
 
@@ -20,20 +24,36 @@ if(DQMMon.onlinePublishing):
     DQMMon.getdeadRocTrendLayer_1()
     DQMMon.refresh()
 
-    print(DQMMon.runinfo['run'])
-    print(DQMMon.runinfo['lumi'])
-    print(DQMMon.runinfo['beamMode'])
-    print(DQMMon.runinfo['run_type'])
-    print(DQMMon.dead_value)
+    #print(DQMMon.runinfo['run'])
+    # print(DQMMon.runinfo['lumi'])
+    # print(DQMMon.runinfo['beamMode'])
+    # print(DQMMon.runinfo['run_type'])
+    # print(DQMMon.dead_value)
 
     if(DQMMon.runinfo['beamMode']=='stable' and DQMMon.runinfo['run_type']=='pp_run' and (DQMMon.dead_value>70)):
-        #Run is written in db only if the sms/email has been sent
+    #Run is written in db only if the sms/email has been sent
         dbcursor.execute('SELECT EXISTS(SELECT 1 FROM alarms WHERE run="%s" )' % DQMMon.runinfo['run'])
-        alarm_handled, = c.fetchone()
-        print(alarm_handled)
-        if(alarm_handled!=1):
+        alarm_handled, = dbcursor.fetchone()
+        
+        if(alarm_handled!=1):  
+            #print("alarm_not_handled")
             dbcursor.execute("INSERT INTO alarms(date, run, lumi, dead_value, DataPresent) VALUES (?, ? , ? , ?, 1);",(datetime.datetime.now(), DQMMon.runinfo['run'], DQMMon.runinfo['lumi'], DQMMon.dead_value))
             conn.commit()
-            #Write the message in txt file(and db?)
-            #Do something to send sms and email
-            #DQMMon.refresh()
+            send_mail(DQMMon)
+            #send_mail(DQMMon, isSMS=True) #sendsms
+
+            ###Checks to avoid spam
+            now = datetime.datetime.now()
+            onehour = datetime.timedelta(hours=1)
+            onehour_ago = now - onehour
+
+            dbcursor.execute("SELECT COUNT(*) FROM alarms WHERE date BETWEEN ? AND ? ", (onehour_ago, now))
+            alarms_in_hour, = dbcursor.fetchone()
+            if(alarms_in_hour>2):
+                send_mail(DQMMon, Text="Application stopped. Number of alarms in an hour exceeded the limit. \nCheck manually.")
+                exit()
+        
+
+
+
+
