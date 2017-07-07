51abac79b0f41fddb5958279e0bb83954fde0e90
gflouris <flourisj@gmail.com>
2017-07-07 15:51:39 +0200

    add parameters

--- script/mimeemail.py
+++ script/mimeemail.py
@@ -1,36 +1,44 @@
-#!/usr/bin/env python -u
-
-import os, sys, re, time, commands, glob
+#import os, sys, re, time, commands, glob
+import os
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
 from email import Encoders
 
-def send_mail(attachement):
+def send_mail(DQMMon, Text = None, isSMS = False, attachement=None):
 
-	emailmessage = open("ReportFromOnlineDQM.txt","r").read()
-	runnumber = open("reference.txt","r").readline()
 	COMMASPACE = ', '
 
+
+	emailmessage = "Alarm from Online DQM\n Run:   "+ str(DQMMon.runinfo['run']) +"\n LumiSection:  "+str(DQMMon.runinfo['lumi']) +"\n Beam Mode:  "+str(DQMMon.runinfo['beamMode'])+"\n Numer of Dead ROC: "+str(DQMMon.dead_value)+"\n DataPresent: True"
+
 	server = "localhost"
-	#        me = 'cctrack@mail.cern.ch'
-	#        me = 'threus@cern.ch'
+	#me = 'cctrack@mail.cern.ch'
 	me = 'gflouris@cern.ch'
-#### 2013 commissioning
-#	topeople = ['threus@cern.ch','cms-tracker-doc@cern.ch','francesco.palmonari@cern.ch','mia.tosi@cern.ch','erik.butz@cern.ch','Christian.Barth@cern.ch']
+    #### List of emails that receive this email
+	#topeople = ['threus@cern.ch','cms-tracker-doc@cern.ch','francesco.palmonari@cern.ch','mia.tosi@cern.ch','erik.butz@cern.ch','Christian.Barth@cern.ch']
 	
 	topeople = ['gflouris@cern.ch']
+
+	if( isSMS ):
+		topeople = ['0041764875503@mail2sms.cern.ch', '0041764872389@mail2sms.cern.ch']
+
+
 	msg = MIMEMultipart()
 	msg['From'] = me
 	msg['To']   = COMMASPACE.join(topeople)
-	msg['Subject'] = 'TKDoc Notification: RUN ' + str(runnumber)	
+	msg['Subject'] = 'TKDoc Notification: RUN ' + str(DQMMon.runinfo['run'])	
 	
+	if(Text!=None):
+		msg['Subject'] = "TKDoc Notification - Application stopped."
+		emailmessage = Text
+
+
 	msg.attach( MIMEText(emailmessage) )
 
-	isnewbadmodule = os.path.getsize('tkmap_bm_input.txt')
 
-	if int(isnewbadmodule) > 0:
+	if (attachement != None and isSMS==False):
 		part = MIMEBase('application', "octet-stream")
 		part.set_payload( open(attachement,"rb").read() )
 		Encoders.encode_base64(part)
@@ -41,9 +49,3 @@ def send_mail(attachement):
 	smtp.sendmail(me, topeople, msg.as_string())
 	smtp.close()
 
-
-ifile_checkemail = open("sendemail.txt","r")
-emailtosend = ifile_checkemail.readline()
-print(emailtosend)
-if int(emailtosend) == 1:
-	send_mail('newbadmodules_tkmap.png')
